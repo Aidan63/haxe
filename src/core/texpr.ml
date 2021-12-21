0b113b26f792ec1733dc2189e88299dc5c316f41
Aidan Lee <aidan.lee63@gmail.com>
2021-12-21 19:16:58 +0000

    very basic binary literal

--- src/core/texpr.ml
+++ src/core/texpr.ml
@@ -570,6 +570,7 @@ let type_constant basic c p =
 	match c with
 	| Int s ->
 		if String.length s > 10 && String.sub s 0 2 = "0x" then typing_error "Invalid hexadecimal integer" p;
+		if String.length s > 34 && String.sub s 0 2 = "0b" then typing_error "Invalid binary integer" p;
 		(try mk (TConst (TInt (Int32.of_string s))) basic.tint p
 		with _ -> mk (TConst (TFloat s)) basic.tfloat p)
 	| Float f -> mk (TConst (TFloat f)) basic.tfloat p
