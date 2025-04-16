f6e2276a780226284fbaee6a450d69b32ae9f8c1
Simon Krajewski <simon@haxe.org>
2025-04-16 11:32:22 +0200

    try something

--- std/haxe/coro/EventLoop.hx
+++ std/haxe/coro/EventLoop.hx
@@ -1,8 +1,12 @@
 package haxe.coro;
 
+#if sys
 import sys.thread.EventLoop;
-
 private typedef EventLoopImpl = sys.thread.EventLoop;
+#else
+import haxe.coro.EventLoopImpl;
+private typedef EventLoopImpl = haxe.coro.EventLoopImpl;
+#end
 
 @:coreApi abstract EventLoop(EventLoopImpl) {
     public function new() {
