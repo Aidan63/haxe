eb68390a1caa3341390a900d1594e10d07065640
Simon Krajewski <simon@haxe.org>
2025-04-20 17:08:50 +0200

    use more precise type on suspend

--- std/haxe/coro/Coroutine.hx
+++ std/haxe/coro/Coroutine.hx
@@ -22,7 +22,7 @@ private class CoroSuspend<T> extends haxe.coro.BaseContinuation<T> {
 @:coreType
 abstract Coroutine<T:haxe.Constraints.Function> {
 	@:coroutine @:coroutine.transformed
-	public static function suspend<T>(func:haxe.coro.IContinuation<Any>->Void, _hx_completion:haxe.coro.IContinuation<Any>):T {
+	public static function suspend<T>(func:haxe.coro.IContinuation<T>->Void, _hx_completion:haxe.coro.IContinuation<Any>):T {
 		var _hx_continuation = new CoroSuspend(_hx_completion);
 		var safe = new haxe.coro.continuations.RacingContinuation(_hx_completion, _hx_continuation);
 		func(safe);
