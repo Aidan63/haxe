012f73655e6bf55e9583740f4130fcf9fccf59d5
Aidan Lee <aidan.lee63@gmail.com>
2025-05-10 15:59:23 +0100

    job awaiting

--- std/haxe/coro/Job.hx
+++ std/haxe/coro/Job.hx
@@ -9,17 +9,20 @@ class Job {
 
     final completionCallbacks : Array<()->Void>;
 
+    var finished : Bool;
+
     var completedChildren : Int;
 
     public var completed (get, never) : Bool;
 
     function get_completed() {
-        return completedChildren == children.length;
+        return finished && completedChildren == children.length;
     }
 
     public function new(parent, callback) {
         this.parent = parent;
         
+        finished            = false;
         children            = [];
         completionCallbacks = [ callback ];
         completedChildren   = 0;
@@ -34,6 +37,8 @@ class Job {
     }
 
     public function complete<T>(v:T) {
+        finished = true;
+
         if (completed) {
             for (callback in completionCallbacks) {
                 callback();
@@ -42,18 +47,20 @@ class Job {
     }
 
     public function completeExceptionally(exn:Exception) {
+        finished = true;
+
         throw new NotImplementedException();
     }
 
-    // @:coroutine public function wait() {
-    //     if (completed) {
-    //         return;
-    //     }
-
-    //     Coroutine.suspend(cont -> {
-    //         onComplete(() -> cont.resume(null, null));
-    //     });
-    // }
+    @:coroutine public function await() {
+        Coroutine.suspend(cont -> {
+            if (completed) {
+                cont.resume(null, null);
+            } else {
+                completionCallbacks.push(() -> cont.resume(null, null));
+            }
+        });
+    }
 
     function onChildCompleted() {
         completedChildren++;
