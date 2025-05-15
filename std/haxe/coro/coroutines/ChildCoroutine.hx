b6ea295fc75b175581cf462f73c408dd04e52562
Aidan Lee <aidan.lee63@gmail.com>
2025-05-15 21:03:28 +0100

    Remove locals which don't give any clarity now

--- std/haxe/coro/coroutines/ChildCoroutine.hx
+++ std/haxe/coro/coroutines/ChildCoroutine.hx
@@ -4,11 +4,8 @@ import haxe.coro.context.Context;
 
 class ChildCoroutine<T> extends BaseCoroutine<T> {
 	public function new(parentContext : Context) {
-		final ctx    = parentContext.clone();
-		final parent = parentContext.get(Coroutine.key);
+		super(parentContext.clone(), parentContext.get(Coroutine.key));
 
-		super(ctx, parent);
-
-		ctx.add(this);
+		context.add(this);
 	}
 }
