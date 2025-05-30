406fcf6eaa8c871886669c891548954d944f075e
Simon Krajewski <simon@haxe.org>
2025-05-30 21:01:32 +0200

    remove mutex

--- std/hxcoro/ds/Channel.hx
+++ std/hxcoro/ds/Channel.hx
@@ -1,7 +1,6 @@
 package hxcoro.ds;
 
 import haxe.coro.IContinuation;
-import haxe.coro.Mutex;
 import hxcoro.Coro.suspend;
 
 @:structInit
@@ -17,8 +16,6 @@ private class SuspendedWrite<T> {
 	from a later call to `read` or `write.
 **/
 class Channel<T> {
-	final lock = new Mutex();
-
 	final maxQueueSize = 3;
 
 	final writeQueue = new Array<T>();
@@ -37,21 +34,16 @@ class Channel<T> {
 		suspended. It can be resumed by a later call to `read`.
 	**/
 	@:coroutine public function write(v:T) {
-		lock.acquire();
-
 		switch suspendedReads.shift() {
 			case null:
 				if (writeQueue.length < maxQueueSize) {
 					writeQueue.push(v);
-					lock.release();
 				} else {
-					lock.release();
 					suspend(cont -> {
 						suspendedWrites.push({v: v, cont: cont});
 					});
 				}
 			case cont:
-				lock.release();
 				cont.resume(v, null);
 		}
 	}
@@ -61,26 +53,18 @@ class Channel<T> {
 		execution is suspended. It can be resumed by a later call to `write`.
 	**/
 	@:coroutine public function read():T {
-		lock.acquire();
-
 		while (writeQueue.length < maxQueueSize && suspendedWrites.length > 0) {
 			final write = suspendedWrites.shift();
-			lock.acquire();
 			writeQueue.push(write.v);
-			lock.release();
 			write.cont.resume(null, null);
 		}
 
 		switch writeQueue.shift() {
 			case null:
-				lock.release();
 				return suspend(cont -> {
-					lock.acquire();
 					suspendedReads.push(cont);
-					lock.release();
 				});
 			case v:
-				lock.release();
 				return v;
 		}
 	}
