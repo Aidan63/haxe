14c80633d73225a64895d2247a06f417c3a1961d
Simon Krajewski <simon@haxe.org>
2025-06-04 06:22:59 +0200

    fix order of operations

--- std/hxcoro/ds/Channel.hx
+++ std/hxcoro/ds/Channel.hx
@@ -26,11 +26,11 @@ private class SuspendedWrite<T> implements IContinuation<T> {
 	public function new(continuation:ICancellableContinuation<T>, value, suspendedWrites:PagedDeque<Any>) {
 		this.continuation = continuation;
 		this.value        = value;
-		continuation.onCancellationRequested = onCancellation ;
 		// writeMutex.acquire();
 		hostPage = suspendedWrites.push(this);
 		hostIndex = suspendedWrites.lastIndex - 1;
 		// writeMutex.release();
+		continuation.onCancellationRequested = onCancellation;
 	}
 
 	public function resume(v:T, error:Exception) {
@@ -65,12 +65,12 @@ class SuspendedRead<T> implements IContinuation<T> {
 
 	public function new(continuation:ICancellableContinuation<T>, suspendedReads:PagedDeque<Any>) {
 		this.continuation = continuation;
-		continuation.onCancellationRequested = onCancellation;
 
 		// readMutex.acquire();
 		hostPage = suspendedReads.push(this);
 		hostIndex = suspendedReads.lastIndex - 1;
 		// readMutex.release();
+		continuation.onCancellationRequested = onCancellation;
 	}
 
 	public function resume(v:T, error:Exception) {
