8c7db3454aa5233abe817089f94e7375068d87b4
Aidan Lee <aidan.lee63@gmail.com>
2025-06-07 12:12:52 +0100

    Bring back int64 bodge I forgot I removed

--- tests/misc/coroutines/src/issues/aidan/Issue126.hx
+++ tests/misc/coroutines/src/issues/aidan/Issue126.hx
@@ -65,7 +65,7 @@ class Issue126 extends utest.Test {
 		final task = CoroRun.with(scheduler).create(node -> {
 			final channel = new Channel(0);
 			@:coroutine function log(s:String) {
-				channel.write('${scheduler.now()}: $s');
+				channel.write('${@:privateAccess scheduler.now().toString()}: $s');
 			}
 			final junction = new Junction(true);
 			final leftChild = node.async(node -> {
