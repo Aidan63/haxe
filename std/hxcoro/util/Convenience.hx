62878370475042b150cbf338410cec3680e05088
Aidan Lee <aidan.lee63@gmail.com>
2025-06-07 11:55:01 +0100

    Document overloads

--- std/hxcoro/util/Convenience.hx
+++ std/hxcoro/util/Convenience.hx
@@ -71,34 +71,77 @@ class Convenience {
 		cont.context.get(Scheduler).scheduleFunction(() -> cont.resume(result, error));
 	}
 
+	/**
+	 * Schedules a function to be executed as soon as possible
+	 * @param scheduler Scheduler to execute the function on.
+	 * @param func Function to execute.
+	 * @return Handle which provides a best effort way to cancel the execution of the scheduled function.
+	 */
 	public static extern inline overload function scheduleFunction(scheduler : Scheduler, func : ()->Void) : ISchedulerHandle {
 		return scheduler.schedule(0, null, (_, _) -> {
 			func();
 		});
 	}
 
+	/**
+	 * Schedules a function to be executed as soon as possible
+	 * @param scheduler Scheduler to execute the function on.
+	 * @param state Object to be passed into the function when executed.
+	 * @param func Function to execute.
+	 * @return Handle which provides a best effort way to cancel the execution of the scheduled function.
+	 */
 	public static extern inline overload function scheduleFunction<T>(scheduler : Scheduler, state : T, func : (state : T)->Void) : ISchedulerHandle {
 		return scheduler.schedule(0, state, (_, s) -> {
 			func(s);
 		});
 	}
 
+	/**
+	 * Schedules a function to be executed as soon as possible
+	 * @param scheduler Scheduler to execute the function on.
+	 * @param state Object to be passed into the function when executed.
+	 * @param func Function to execute.
+	 * @return Handle which provides a best effort way to cancel the execution of the scheduled function.
+	 */
 	public static extern inline overload function scheduleFunction<T>(scheduler : Scheduler, state : T, func : (scheduler : Scheduler, state : T)->Void) : ISchedulerHandle {
 		return scheduler.schedule(0, state, func);
 	}
 
+	/**
+	 * Schedules a function to be executed after the specified time has passed.
+	 * @param scheduler Scheduler to execute the function on.
+	 * @param ms The relative time in milliseconds after which to execute the function.
+	 * @param func Function to execute.
+	 * @return Handle which provides a best effort way to cancel the execution of the scheduled function.
+	 */
 	public static extern inline overload function scheduleFunction(scheduler : Scheduler, ms : Int64, func : ()->Void) : ISchedulerHandle {
 		return scheduler.schedule(ms, null, (_, _) -> {
 			func();
 		});
 	}
 
+	/**
+	 * Schedules a function to be executed after the specified time has passed.
+	 * @param scheduler Scheduler to execute the function on.
+	 * @param ms The relative time in milliseconds after which to execute the function.
+	 * @param state Object to be passed into the function when executed.
+	 * @param func Function to execute.
+	 * @return Handle which provides a best effort way to cancel the execution of the scheduled function.
+	 */
 	public static extern inline overload function scheduleFunction<T>(scheduler : Scheduler, ms : Int64, state : T, func : (state : T)->Void) : ISchedulerHandle {
 		return scheduler.schedule(ms, state, (_, s) -> {
 			func(s);
 		});
 	}
 
+	/**
+	 * Schedules a function to be executed after the specified time has passed.
+	 * @param scheduler Scheduler to execute the function on.
+	 * @param ms The relative time in milliseconds after which to execute the function.
+	 * @param state Object to be passed into the function when executed.
+	 * @param func Function to execute.
+	 * @return Handle which provides a best effort way to cancel the execution of the scheduled function.
+	 */
 	public static extern inline overload function scheduleFunction<T>(scheduler : Scheduler, ms : Int64, state : T, func : (scheduler : Scheduler, state : T)->Void) : ISchedulerHandle {
 		return scheduler.schedule(ms, state, func);
 	}
