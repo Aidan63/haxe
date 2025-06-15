a8fcc0ea78503f9d779c3332b85f89b178d1fb4e
Simon Krajewski <simon@haxe.org>
2025-06-15 11:30:51 +0200

    add some more tests

--- tests/misc/coroutines/src/issues/aidan/Issue153.hx
+++ tests/misc/coroutines/src/issues/aidan/Issue153.hx
@@ -48,4 +48,87 @@ class Issue153 extends utest.Test {
 		Assert.equals(6, d.pop());
 		Assert.isTrue(d.isEmpty());
 	}
+
+	function createTwoPageDeck(pageSize:Int) {
+		var d:PagedDeque<Any> = new PagedDeque(pageSize);
+		final pages = [
+			for (i in 0...pageSize << 1) {
+				d.push(i);
+			}
+		];
+		return {
+			deque: d,
+			pages: pages
+		}
+	}
+
+	public function testBounds1() {
+		final data = createTwoPageDeck(1);
+		final pages = data.pages;
+		final d = data.deque;
+		Assert.notEquals(pages[0], pages[1]);
+		Assert.isFalse(pages[0].delete(1));
+		Assert.isFalse(pages[1].delete(0));
+		// delete last, then push
+		Assert.isTrue(pages[1].delete(1));
+		d.push(2);
+		Assert.equals(0, d.pop());
+		Assert.equals(2, d.pop());
+		Assert.isTrue(d.isEmpty());
+	}
+
+	public function testBounds2() {
+		final data = createTwoPageDeck(2);
+		final pages = data.pages;
+		final d = data.deque;
+		Assert.equals(pages[0], pages[1]);
+		Assert.equals(pages[2], pages[3]);
+		Assert.notEquals(pages[0], pages[2]);
+		Assert.isFalse(pages[0].delete(2));
+		Assert.isFalse(pages[0].delete(3));
+		Assert.isFalse(pages[2].delete(0));
+		Assert.isFalse(pages[2].delete(1));
+		// delete first and last
+		Assert.isTrue(pages[0].delete(0));
+		Assert.isTrue(pages[2].delete(3));
+		Assert.equals(1, d.pop());
+		Assert.equals(2, d.pop());
+		Assert.isTrue(d.isEmpty());
+	}
+
+	public function testBounds3() {
+		final data = createTwoPageDeck(3);
+		final pages = data.pages;
+		final d = data.deque;
+		// delete middle
+		Assert.isTrue(pages[0].delete(1));
+		Assert.isTrue(pages[3].delete(4));
+		Assert.equals(0, d.pop());
+		Assert.equals(2, d.pop());
+		Assert.equals(3, d.pop());
+		Assert.equals(5, d.pop());
+		Assert.isTrue(d.isEmpty());
+	}
+
+	public function testWildDeletion() {
+		final data = createTwoPageDeck(100);
+		final pages = data.pages;
+		final page1 = pages[0];
+		final page2 = pages[100];
+		final d = data.deque;
+		final values = [for (i in 0...200) i];
+		values.sort((_, _) -> Math.random() > 0.5 ? 1 : -1);
+		Assert.isFalse(d.isEmpty());
+		for (i in values) {
+			switch [page1.delete(i), page2.delete(i)] {
+				case [true, false] | [false, true]:
+				case [true, true]:
+					Assert.fail('Deleted $i from two pages');
+				case [false, false]:
+					Assert.fail('Couldn\'t delete $i from any page');
+			}
+		}
+		// Assert.isTrue(d.isEmpty()); // this is still broken
+		Assert.pass();
+	}
 }
