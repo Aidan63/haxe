f367a4b38176052440549d85cba1c37de75d2db5
Aidan Lee <aidan.lee63@gmail.com>
2025-06-28 23:12:57 +0100

    Quickly thrown together and untested ubbounded channel

--- std/hxcoro/ds/channels/Channel.hx
+++ std/hxcoro/ds/channels/Channel.hx
@@ -10,14 +10,12 @@ import hxcoro.ds.channels.bounded.BoundedWriter;
 import hxcoro.ds.channels.bounded.SingleBoundedReader;
 import hxcoro.ds.channels.bounded.SingleBoundedWriter;
 import hxcoro.ds.channels.bounded.BoundedChannel;
+import hxcoro.ds.channels.unbounded.UnboundedReader;
+import hxcoro.ds.channels.unbounded.UnboundedWriter;
+import hxcoro.ds.channels.unbounded.UnboundedChannel;
 import hxcoro.concurrent.AtomicObject;
 
 typedef DropCallback<T> = (dropped : T)->Void;
-
-enum ChannelKind {
-	Bounded(size : Int);
-	Unbounded;
-}
 
 enum FullBehaviour<T> {
 	Wait;
@@ -26,14 +24,16 @@ enum FullBehaviour<T> {
 	DropWrite(f : DropCallback<T>);
 }
 
-typedef ChannelOptions<T> = {
-	var kind : ChannelKind;
-
-	var ?writeBehaviour : FullBehaviour<T>;
-
+typedef ChannelOptions = {
 	var ?singleReader : Bool;
 
 	var ?singleWriter : Bool;
+}
+
+typedef BoundedChannelOptions<T> = ChannelOptions & {
+	var size : Int;
+
+	var ?writeBehaviour : FullBehaviour<T>;
 }
 
 abstract class Channel<T> {
@@ -47,40 +47,45 @@ abstract class Channel<T> {
 		this.writer = writer;
 	}
 
-	public static function create<T>(options : ChannelOptions<T>):Channel<T> { 
-		switch options.kind {
-			case Bounded(size):
-				if (size < 1) {
-					throw new ArgumentException("size");
-				}
-
-				final closed         = new Out();
-				final singleReader   = options.singleReader ?? false;
-				final singleWriter   = options.singleWriter ?? false;
-				final writeBehaviour = options.writeBehaviour ?? Wait;
-
-				if (singleReader && singleWriter && writeBehaviour.match(DropNewest(_)) == false && writeBehaviour.match(DropOldest(_)) == false) {
-					final buffer      = new ConcurrentCircularBuffer(size);
-					final readWaiter  = new AtomicObject<IContinuation<Bool>>(null);
-					final writeWaiter = new AtomicObject<IContinuation<Bool>>(null);
-					
-					return
-						new BoundedChannel(
-							new SingleBoundedReader(buffer, writeWaiter, readWaiter, closed),
-							new SingleBoundedWriter(buffer, writeWaiter, readWaiter, closed, writeBehaviour));
-				} else {
-					final buffer       = [];
-					final readWaiters  = new PagedDeque();
-					final writeWaiters = new PagedDeque();
-
-					return
-						new BoundedChannel(
-							new BoundedReader(buffer, size, writeWaiters, readWaiters, closed),
-							new BoundedWriter(buffer, size, writeWaiters, readWaiters, closed, writeBehaviour));
-				}
-
-			case Unbounded:
-				throw new NotImplementedException();
+	public static function createBounded<T>(options : BoundedChannelOptions<T>):Channel<T> { 
+		if (options.size < 1) {
+			throw new ArgumentException("size");
 		}
+
+		final closed         = new Out();
+		final singleReader   = options.singleReader ?? false;
+		final singleWriter   = options.singleWriter ?? false;
+		final writeBehaviour = options.writeBehaviour ?? Wait;
+
+		if (singleReader && singleWriter && writeBehaviour.match(DropNewest(_)) == false && writeBehaviour.match(DropOldest(_)) == false) {
+			final buffer      = new ConcurrentCircularBuffer(options.size);
+			final readWaiter  = new AtomicObject<IContinuation<Bool>>(null);
+			final writeWaiter = new AtomicObject<IContinuation<Bool>>(null);
+			
+			return
+				new BoundedChannel(
+					new SingleBoundedReader(buffer, writeWaiter, readWaiter, closed),
+					new SingleBoundedWriter(buffer, writeWaiter, readWaiter, closed, writeBehaviour));
+		} else {
+			final buffer       = [];
+			final readWaiters  = new PagedDeque();
+			final writeWaiters = new PagedDeque();
+
+			return
+				new BoundedChannel(
+					new BoundedReader(buffer, options.size, writeWaiters, readWaiters, closed),
+					new BoundedWriter(buffer, options.size, writeWaiters, readWaiters, closed, writeBehaviour));
+		}
+	}
+
+	public static function createUnbounded<T>(options : ChannelOptions):Channel<T> {
+		final closed      = new Out();
+		final buffer      = new PagedDeque();
+		final readWaiters = new PagedDeque();
+
+		return
+			new UnboundedChannel(
+				new UnboundedReader(buffer, readWaiters, closed),
+				new UnboundedWriter(buffer, readWaiters, closed));
 	}
 }
---
+++ std/hxcoro/ds/channels/unbounded/UnboundedWriter.hx
@@ -0,0 +1,65 @@
+package hxcoro.ds.channels.unbounded;
+
+import hxcoro.ds.Out;
+import hxcoro.ds.PagedDeque;
+import haxe.coro.IContinuation;
+
+using hxcoro.util.Convenience;
+
+final class UnboundedWriter<T> implements IChannelWriter<T> {
+	final closed : Out<Bool>;
+
+	final buffer : PagedDeque<T>;
+
+	final readWaiters : PagedDeque<IContinuation<Bool>>;
+
+	public function new(buffer, readWaiters, closed) {
+		this.buffer      = buffer;
+		this.readWaiters = readWaiters;
+		this.closed      = closed;
+	}
+
+	public function tryWrite(v:T):Bool {
+		if (closed.get()) {
+			return false;
+		}
+
+		final _ = buffer.push(v);
+
+		final cont = new Out();
+		while (readWaiters.tryPop(cont)) {
+			cont.get().succeedAsync(true);
+		}
+
+		return true;
+	}
+
+	@:coroutine public function waitForWrite():Bool {
+		if (closed.get()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@:coroutine public function write(v:T) {
+		while (waitForWrite()) {
+			if (tryWrite(v)) {
+				return;
+			}
+		}
+	}
+
+	public function close() {
+		if (closed.get()) {
+			return;
+		}
+
+		closed.set(true);
+
+		final cont = new Out();
+		while (readWaiters.tryPop(cont)) {
+			cont.get().succeedAsync(false);
+		}
+	}
+}
