2dab025d1a95cd2b1cac8b24bd04ab7e466c4b00
Aidan Lee <aidan.lee63@gmail.com>
2025-09-07 15:23:03 +0100

    disable single producer consumer implementation
    will revisit at a later time

--- std/hxcoro/ds/channels/Channel.hx
+++ std/hxcoro/ds/channels/Channel.hx
@@ -52,29 +52,30 @@ abstract class Channel<T> {
 		}
 
 		final closed         = new Out();
-		final singleReader   = options.singleReader ?? false;
-		final singleWriter   = options.singleWriter ?? false;
 		final writeBehaviour = options.writeBehaviour ?? Wait;
-
-		if (singleReader && singleWriter && writeBehaviour.match(DropNewest(_)) == false && writeBehaviour.match(DropOldest(_)) == false) {
-			final buffer      = new ConcurrentCircularBuffer(options.size);
-			final readWaiter  = new AtomicObject<IContinuation<Bool>>(null);
-			final writeWaiter = new AtomicObject<IContinuation<Bool>>(null);
+		
+		// TODO : Revisit this single consumer producer implementation once we have threading in and can make some comparisons.
+		// final singleReader   = options.singleReader ?? false;
+		// final singleWriter   = options.singleWriter ?? false;
+		// if (singleReader && singleWriter && writeBehaviour.match(DropNewest(_)) == false && writeBehaviour.match(DropOldest(_)) == false) {
+		// 	final buffer      = new ConcurrentCircularBuffer(options.size);
+		// 	final readWaiter  = new AtomicObject<IContinuation<Bool>>(null);
+		// 	final writeWaiter = new AtomicObject<IContinuation<Bool>>(null);
 			
-			return
-				new BoundedChannel(
-					new SingleBoundedReader(buffer, writeWaiter, readWaiter, closed),
-					new SingleBoundedWriter(buffer, writeWaiter, readWaiter, closed, writeBehaviour));
-		} else {
-			final buffer       = [];
-			final readWaiters  = new PagedDeque();
-			final writeWaiters = new PagedDeque();
+		// 	return
+		// 		new BoundedChannel(
+		// 			new SingleBoundedReader(buffer, writeWaiter, readWaiter, closed),
+		// 			new SingleBoundedWriter(buffer, writeWaiter, readWaiter, closed, writeBehaviour));
+		// }
 
-			return
-				new BoundedChannel(
-					new BoundedReader(buffer, options.size, writeWaiters, readWaiters, closed),
-					new BoundedWriter(buffer, options.size, writeWaiters, readWaiters, closed, writeBehaviour));
-		}
+		final buffer       = [];
+		final readWaiters  = new PagedDeque();
+		final writeWaiters = new PagedDeque();
+
+		return
+			new BoundedChannel(
+				new BoundedReader(buffer, options.size, writeWaiters, readWaiters, closed),
+				new BoundedWriter(buffer, options.size, writeWaiters, readWaiters, closed, writeBehaviour));
 	}
 
 	public static function createUnbounded<T>(options : ChannelOptions):Channel<T> {
